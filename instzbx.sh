96ea5d9d66d37bf973a473f13eb36ed29b061b7e
Vinicius-pinh <73889866+Vinicius-pinh@users.noreply.github.com>
2020-11-19 08:16:42 -0300

    Update instzbx.sh

--- instzbx.sh
+++ instzbx.sh
@@ -78,7 +78,9 @@ fi
 ##################
 
 ZBXFILE="/etc/zabbix/zabbix_agentd.conf"
-[[ -a "$ZBXFILE" ]]
+
+if [[ -a "$ZBXFILE" ]]; then 
+
 service zabbix-agent stop
 
 #####################
@@ -90,7 +92,7 @@ service zabbix-agent stop
 IP=`ifconfig | egrep -o '(inet addr: | inet | addr:)([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' | egrep -o '[[:digit:]].*'`
 IPV4GET=`echo $IP | cut -d " " -f "1"`
 RANGE=`echo $IPV4GET | cut -d "." -f "3"`
-PROXYIP=`echo "172.27.$RANGE.128"`
+PROXYIP=`echo "192.168.$RANGE.128"`
 
 HOSTNAME=`hostname`
 #ZBXFILE="/etc/zabbix/zabbix_agentd.conf"
@@ -124,3 +126,6 @@ chown zabbix:zabbix /opt/psk.key
 service zabbix-agent start
 
 ps aux | grep zabbix
+else
+echo "Please, check zabbix agent instalation"
+fi 
